a3650b37e719793c19298e8d7d79b7a5868d9d31
Sunil Patel <snlpatel001213@hotmail.com>
2021-12-14 16:49:10 +0530

    DB connection updated

---
+++ Assets/AirplanePhysics/Code/Scripts/Database/DB_InputClassDefinitions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using SQLite4Unity3d;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DB_InputClassDefinitions 
+{
+	/// <summary>
+	/// Just [PrimaryKey] is added to the Id Attribute as we only want the updated value and dont want to accumulate it
+	/// if one need to auto increment it the Id fielsd can be configured as [PrimaryKey, AutoIncrement]
+	/// In this case it will fill up the database. [Not supported]
+	/// </summary>
+	/// <value></value>
+    [PrimaryKey]
+	public string direction  { get; set; }
+    public DateTime date  { get; set; }
+	public string Name { get; set; }
+	public string Surname { get; set; }
+	public int Age { get; set; }
+
+}
+
---
+++ Assets/AirplanePhysics/Code/Scripts/Database/DB_Test.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using SQLite4Unity3d;
+using System.IO;
+using AirControl;
+
+
+public class DB_Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public SQLiteConnection _connection ;
+    void Start()
+    {
+        string persistentDataPath = Application.streamingAssetsPath;
+        string airControlVersion = CommonConfigs.GET_VERSION();
+        string DatabaseName =  "AirControl-"+airControlVersion+".sqlite";
+        string dbPath = System.IO.Path.Combine(persistentDataPath, DatabaseName);
+        
+        if(File.Exists(dbPath))
+        {
+            _connection = new SQLiteConnection(dbPath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+        }
+        else
+        {
+            _connection = DB_InitDatabase.GetConnection(persistentDataPath,DatabaseName);
+            DB_InitDatabase.CreateTable<DB_InputClassDefinitions>( _connection);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DB_InputClassDefinitions temp = new DB_InputClassDefinitions{
+				direction = "outgoing",
+                date = DateTime.Now,
+				Name = "Sunil",
+				Surname = "Sivan",
+				Age = 27};
+        _connection.InsertOrReplace(temp);
+
+    }
+}
