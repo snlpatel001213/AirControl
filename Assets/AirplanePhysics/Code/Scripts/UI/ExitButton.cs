726474e37d32130a4b5c80bdc5836525ff3cc740
snlpatel001213 <snlpatel001213@hotmail.com>
2022-02-18 10:43:18 +0530

    Added exit button on UI Added town

---
+++ Assets/AirplanePhysics/Code/Scripts/UI/ExitButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Exit(){
+        Debug.Log("Exit Triggered");
+        Application.Quit();
+#if UNITY_EDITOR	
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+
+    }
+}
