728bc4a8aa60b38bae98540e5362aaba9859b967
snlpatel001213 <snlpatel001213@hotmail.com>
2022-03-03 18:47:59 +0530

    Magnetometer code stub added

---
+++ Assets/AirplanePhysics/Code/Scripts/UI/Instruments/AC_Airplane_Magnetometer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// https://gamedev-resources.com/create-a-compass-with-ugui/
+/// </summary>
+public class AC_Airplane_Magnetometer : MonoBehaviour
+{
+    // void OnGUI()
+    // {
+    //     GUILayout.Label("Magnetometer reading: " + Input.compass.rawVector.ToString());
+    //     Debug.Log(Quaternion.Euler(0, -Input.compass.magneticHeading, 0));
+    // }
+}
