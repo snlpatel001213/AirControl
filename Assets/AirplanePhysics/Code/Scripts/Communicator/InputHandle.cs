a3f7291e4cd15438b171f417e97a60777749d5d7
snlpatel001213 <snlpatel001213@hotmail.com>
2022-03-04 13:32:09 +0530

    FPS Counter added

--- Assets/AirplanePhysics/Code/Scripts/Communicator/InputHandle.cs
+++ Assets/AirplanePhysics/Code/Scripts/Communicator/InputHandle.cs
@@ -26,7 +26,6 @@ namespace Communicator
         public void ParseInput(JObject inputJson)
         {
                 string MsgType = inputJson["MsgType"].ToString();
-                Debug.Log(">>>>>>>>>>>>>>"+inputJson);
                 #region ControlInput
                 if (MsgType=="ControlInput")
                 {
