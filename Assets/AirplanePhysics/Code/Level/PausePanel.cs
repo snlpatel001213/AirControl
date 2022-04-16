759d0999d49e8d7df02505afc66b82858564259f
snlpatel001213 <snlpatel001213@hotmail.com>
2022-04-16 16:46:43 +0530

    Gitpod support added

--- Assets/AirplanePhysics/Code/Level/PausePanel.cs
+++ Assets/AirplanePhysics/Code/Level/PausePanel.cs
@@ -1,35 +1,36 @@
-public class Pause : MonoBehaviour 
-{
-    [SerializeField] private GameObject PausePanel;
-    void Start()
-    {
-        pausePanel.SetActive(false);
-    }
-    void Update()
-    {
-        if(Input.GetKeyDown (KeyCode.Escape)) 
-        {
-            if (!pausePanel.activeInHierarchy) 
-            {
-                PauseGame();
-            }
-            if (pausePanel.activeInHierarchy) 
-            {
-                 ContinueGame();   
-            }
-        } 
-    }
-    private void PauseGame()
-    {
-        Time.timeScale = 0;
-        pausePanel.SetActive(true);
-        //Disable scripts that still work while timescale is set to 0
-    } 
-    private void ContinueGame()
-    {
-        Time.timeScale = 1;
-        pausePanel.SetActive(false);
-        //enable the scripts again
-    }
-}
+// using UnityEngine;
+// public class Pause : MonoBehaviour 
+// {
+//     [SerializeField] private GameObject PausePanel;
+//     void Start()
+//     {
+//         // pausePanel.SetActive(false);
+//     }
+//     void Update()
+//     {
+//         if(Input.GetKeyDown (KeyCode.Escape)) 
+//         {
+//             if (!pausePanel.activeInHierarchy) 
+//             {
+//                 PauseGame();
+//             }
+//             if (pausePanel.activeInHierarchy) 
+//             {
+//                  ContinueGame();   
+//             }
+//         } 
+//     }
+//     private void PauseGame()
+//     {
+//         Time.timeScale = 0;
+//         // pausePanel.SetActive(true);
+//         //Disable scripts that still work while timescale is set to 0
+//     } 
+//     private void ContinueGame()
+//     {
+//         Time.timeScale = 1;
+//         // pausePanel.SetActive(false);
+//         //enable the scripts again
+//     }
+// }
  
